abf478996b0561c91ff4e77d2a6977e4835fc239
slagter@bijbrengen.nl <slagter@bijbrengen.nl>
2023-10-18 16:49:25 +0200

    Extended the Skill class to support prerequisite skills (pre_skills) to orden as freely as possible the unit

--- Skill.py
+++ Skill.py
@@ -1,7 +1,11 @@
 class Skill:
     def __init__(self, name):
         self.name = name
+        self.pre_skills = []
         self.sub_skills = []
+
+    def add_pre_skill(self, skill):
+        self.pre_skills.append(skill)
 
     def add_sub_skill(self, skill):
         self.sub_skills.append(skill)
