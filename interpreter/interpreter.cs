7904e7bccfcb19ec40e96b01697286e31d764d9e
slagter@bijbrengen.nl <slagter@bijbrengen.nl>
2023-10-19 19:43:54 +0200

    Started the main Interpreter class a frame for the interrpeter functionality.

--- interpreter/interpreter.cs
+++ interpreter/interpreter.cs
@@ -1,7 +1,56 @@
-﻿namespace interpreter
-{
-    public class interpreter
-    {
+﻿using System;
+using Irony.Parsing;
 
+namespace Freell
+{
+    public class Interpreter
+    {
+        // Define the grammar of the Freell language using Irony
+        private readonly LanguageData _languageData;
+
+        public Interpreter()
+        {
+            // Initialize the grammar of the Freell language
+            var grammar = new FreellGrammar();
+            _languageData = new LanguageData(grammar);
+        }
+
+        //  Interpret can return a nullable object
+        public object? Interpret(string sourceCode)
+        {
+            // Create a parser for the Freell language
+            var parser = new Parser(_languageData);
+
+            // Parse the source code
+            var parseTree = parser.Parse(sourceCode);
+            if (parseTree.HasErrors())
+            {
+                throw new Exception("Syntax errors found in source code.");
+            }
+
+            // Traverse the parse tree and evaluate expressions,
+            // This is where the Freell interpreter logic goes
+            var result = Evaluate(parseTree.Root);
+            return result;
+        }
+
+        //  Evaluate can return a nullable object
+        private object? Evaluate(ParseTreeNode node)
+        {
+            // Evaluate the node based on its type and value,
+            // and recursively evaluate its child nodes
+            // ...
+            return null;
+        }
+    }
+
+    // Define the grammar of the Freell language
+    public class FreellGrammar : Grammar
+    {
+        public FreellGrammar() : base(false)  // case insensitive
+        {
+            // Define your grammar here
+            // ...
+        }
     }
 }
