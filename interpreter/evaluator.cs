17428e124824141d45d1a08409da46cf75fc4899
slagter@bijbrengen.nl <slagter@bijbrengen.nl>
2023-10-19 20:14:40 +0200

    The grammar starts with the very simple language of the form: identifier = new type;

---
+++ interpreter/evaluator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Irony.Parsing;
+
+namespace Freell
+{
+    public class Evaluator
+    {
+        public object? Evaluate(ParseTreeNode node)
+        {
+            switch (node.Term.Name)
+            {
+                case "declaration":
+                    return EvaluateDeclaration(node);
+                // ... handle other non-terminals ...
+                default:
+                    throw new Exception($"Unknown node type: {node.Term.Name}");
+            }
+        }
+
+        private object? EvaluateDeclaration(ParseTreeNode node)
+        {
+            var identifier = node.ChildNodes[0].Token.Text;
+            var type = node.ChildNodes[3].ChildNodes[0].Term.Name;
+            // ... create a new instance of the specified type and associate it with the identifier ...
+            return null; // Replace with actual logic
+        }
+
+        // ... additional methods to evaluate other non-terminals ...
+    }
+}
---
+++ interpreter/grammar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Irony.Parsing;
+
+namespace Freell
+{
+    // Define the grammar of the Freell language
+    // The grammar starts with the very simple language of the form: identifier = new type;
+    // where "type" can be any of the defined terminal types like "Action," "Actor," etc.
+    public class FreellGrammar : Grammar
+    {
+        public FreellGrammar() : base(false)  // false for case sensitivity
+        {
+            // Define Terminals
+            var identifier = new IdentifierTerminal("identifier");
+            var action = ToTerm("Action");
+            var actor = ToTerm("Actor");
+            var method = ToTerm("Method");
+            var prerequisite = ToTerm("Prerequisite");
+            var resource = ToTerm("Resource");
+            var skill = ToTerm("Skill");
+            var unit = ToTerm("Unit");
+            var assignment = ToTerm("=");
+            var terminator = ToTerm(";");
+
+            // Define Non-terminals
+            var statement = new NonTerminal("statement");
+            var type = new NonTerminal("type");
+            var declaration = new NonTerminal("declaration");
+
+            // Define Rules
+            type.Rule = action | actor | method | prerequisite | resource | skill | unit;
+            declaration.Rule = identifier + assignment + "new" + type + terminator;
+            statement.Rule = declaration;
+
+            // Set the root non-terminal
+            this.Root = statement;
+        }
+    }
+}
