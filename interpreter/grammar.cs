b8580055bf03a20fddf122e18831785bdd5df8d0
slagter@bijbrengen.nl <slagter@bijbrengen.nl>
2023-10-22 23:43:23 +0200

    Added Prerequisite as a Type for the mandatory first statement

--- interpreter/grammar.cs
+++ interpreter/grammar.cs
@@ -13,6 +13,7 @@ namespace Freell
             var actor = ToTerm("Actor");
             var method = ToTerm("Method");
             var portfolio = ToTerm("Portfolio");
+            var prerequisite = ToTerm("Prerequisite");
             var a = ToTerm("a");
             var an = ToTerm("an");
             var period = ToTerm(".");  // Terminal for period
@@ -25,7 +26,7 @@ namespace Freell
 
             // Define Rules
             article.Rule = a | an;
-            type.Rule = action | actor | method | portfolio;
+            type.Rule = action | actor | method | portfolio | prerequisite ;
 
             // Update the BNF-like rule for the start statement to include period
             startStatement.Rule = ToTerm("The root is") + article + type + ToTerm("called") + identifier + period;
@@ -40,7 +41,7 @@ namespace Freell
                 var typeNode = parseNode.ChildNodes[2];
 
                 if ((articleNode.Term.Name == "a" && (typeNode.Term.Name == "Action" || typeNode.Term.Name == "Actor")) ||
-                    (articleNode.Term.Name == "an" && (typeNode.Term.Name == "Method" || typeNode.Term.Name == "Portfolio")))
+                    (articleNode.Term.Name == "an" && (typeNode.Term.Name == "Method" || typeNode.Term.Name == "Portfolio" || typeNode.Term.Name == "Prerequisite")))
                 {
                     // Error: Incorrect article used
                     context.AddParserMessage(new ParserMessage(ParserErrorLevel.Error, parseNode.Span.Location, "Incorrect article used with type."));
